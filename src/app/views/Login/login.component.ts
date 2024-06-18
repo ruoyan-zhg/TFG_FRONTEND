15b532a1b525f37474605a1b89a1a004b816a055
ruoyan-zhg <yy.zhg@icloud.com>
2024-06-18 16:11:49 +0200

    login y register funcionando

--- src/app/views/Login/login.component.ts
+++ src/app/views/Login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  email: string = '';
+  contrasena: string = '';
+  successMessage: string = '';
+  errorMessage: string = '';
+  showModal: boolean = false;
+  modalMessage: string = '';
 
+  constructor(private http: HttpClient, private router: Router) {}
+
+  enviarFormularioLogin(): void {
+    this.http.post<any>('http://localhost:8000/api/login', {
+      email: this.email,
+      contrasena: this.contrasena
+    }).subscribe(
+      response => {
+        console.log(response);
+        this.showMessage('Se ha iniciado sesión correctamente');
+        // Navegar a otra página si es necesario
+        // this.router.navigate(['/ruta-deseada']);
+      },
+      error => {
+        console.error(error);
+        this.showMessage('Error en el inicio de sesión');
+      }
+    );
+  }
+
+  showMessage(message: string): void {
+    this.modalMessage = message;
+    this.showModal = true;
+  }
+
+  closeModal(): void {
+    this.showModal = false;
+  }
 }
--- src/app/views/Registro/registro.component.ts
+++ src/app/views/Registro/registro.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-registro',
@@ -6,5 +7,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./registro.component.scss']
 })
 export class RegistroComponent {
+  nombre: string = '';
+  email: string = '';
+  contrasena: string = '';
+  successMessage: string = '';
+  errorMessage: string = '';
+  showModal: boolean = false;
+  modalMessage: string = '';
 
+  constructor(private http: HttpClient) {}
+
+  enviarFormularioRegistro(): void {
+    this.http.post<any>('http://localhost:8000/api/register', {
+      nombre: this.nombre,
+      email: this.email,
+      contrasena: this.contrasena
+    }).subscribe(
+      response => {
+        console.log(response);
+        this.showMessage('Registrado correctamente');
+      },
+      error => {
+        console.error(error);
+        if (error.status === 409) {
+          this.showMessage('El correo electrónico ya está registrado.');
+        } else {
+          this.showMessage('Algo ha ido mal');
+        }
+      }
+    );
+  }
+
+  showMessage(message: string): void {
+    this.modalMessage = message;
+    this.showModal = true;
+  }
+
+  closeModal(): void {
+    this.showModal = false;
+  }
 }
